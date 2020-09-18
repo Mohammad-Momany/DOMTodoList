69c7a21eb40042a6f203f359bae141fbe0ac91e0
mohammad.w.momani <mohammad.w.momani@gmail.com>
2020-09-18 13:48:52 +0300

    finish add new todo list

---
+++ m.js
@@ -0,0 +1,45 @@
+const todoInput = document.querySelector(".todo-input")
+const todoButton = document.querySelector(".todo-button")
+const todoList = document.querySelector(".todo-list")
+
+todoButton.addEventListener("click", addTodo)
+todoList.addEventListener("click",deleteCheck)
+
+function addTodo(event){
+    event.preventDefault();
+    const todoDiv = document.createElement("div")
+    todoDiv.classList.add("todo") 
+
+
+    const newTodo = document.createElement("li")
+    newTodo.innerHTML = todoInput.value
+    newTodo.classList.add("todo-item")
+    todoDiv.appendChild(newTodo)
+
+
+    const completedButton = document.createElement("button")
+    completedButton.innerHTML = '<i class="fas fa-check"></i>'
+    completedButton.classList.add("complete")
+    todoDiv.appendChild(completedButton)
+
+
+    const trashButton = document.createElement("button")
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>'
+    trashButton.classList.add("trash")
+    todoDiv.appendChild(trashButton)
+    todoList.appendChild(todoDiv)
+    todoInput.value = "";
+}    
+
+function deleteCheck(e){
+    const item = e.target
+    if(item.classList[0] === "trash"){
+        const todo = item.parentElement;
+        todo.remove();
+       
+    }
+    if(item.classList[0] === "complete"){
+        const todo = item.parentElement
+        todo.classList.toggle("completed");
+    }
+}
