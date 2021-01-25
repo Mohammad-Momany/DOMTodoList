edbe8af7da54bd4811c09639881a40c760466cda
Mohammad-W-Momani <mohammad.w.momani@gmail.com>
2021-01-25 21:29:19 +0200

    add error when input empety

--- DOMTodoList.js
+++ DOMTodoList.js
@@ -1,7 +1,7 @@
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
-
+const small = document.querySelector("small");
 todoButton.addEventListener("click", addTodo);
 todoList.addEventListener("click", deleteCheck);
 
@@ -11,31 +11,39 @@ function addTodo(event) {
   todoDiv.classList.add("todo");
 
   const newTodo = document.createElement("li");
-  newTodo.innerHTML = todoInput.value;
-  newTodo.classList.add("todo-item");
-  todoDiv.appendChild(newTodo);
+  if (todoInput.value.length === 0) {
+    small.classList = "error";
+    todoInput.classList = "todo-input-error";
+    return;
+  } else {
+    small.classList = "success";
+    todoInput.classList = "todo-input";
+    newTodo.innerHTML = todoInput.value;
+    newTodo.classList.add("todo-item");
+    todoDiv.appendChild(newTodo);
 
-  const completedButton = document.createElement("button");
-  completedButton.innerHTML = '<i class="fas fa-check"></i>';
-  completedButton.classList.add("complete");
-  todoDiv.appendChild(completedButton);
+    const completedButton = document.createElement("button");
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add("complete");
+    todoDiv.appendChild(completedButton);
 
-  const trashButton = document.createElement("button");
-  trashButton.innerHTML = '<i class="fas fa-trash"></i>';
-  trashButton.classList.add("trash");
-  todoDiv.appendChild(trashButton);
-  todoList.appendChild(todoDiv);
-  todoInput.value = "";
+    const trashButton = document.createElement("button");
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add("trash");
+    todoDiv.appendChild(trashButton);
+    todoList.appendChild(todoDiv);
+    todoInput.value = "";
+  }
 }
 
 function deleteCheck(e) {
   const item = e.target;
+
   if (item.classList[0] === "trash") {
     const todo = item.parentElement;
     todo.remove();
   }
   if (item.classList[0] === "complete") {
-    const todo = item.parentElement;
     todo.classList.toggle("completed");
   }
 }
